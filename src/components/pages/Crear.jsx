5e47dda579e3c832761e8b436db359bb7fb15c11
Nicolas Leyes <nicolasleyesadmi@gmail.com>
2024-09-02 20:00:17 -0300

    tercer comit de proyecto frontend blog 30-08

--- src/components/pages/Crear.jsx
+++ src/components/pages/Crear.jsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { useForm } from '../../hooks/useForm';
+import { Peticion } from '../../helpers/Peticion';
+import { Global } from '../../helpers/Global';
 
 export const Crear = () => {
+
+  const {formulario, enviado, cambiado} = useForm({});
+
+  const [resultado, setResultado] = useState("no_enviado");
+
+  const guardarArticulo = async(e) => {
+    e.preventDefault();
+
+
+    //recoger datos formularios
+    let nuevoArticulo = formulario;
+
+    //guardar articulos en el backen
+    const {datos, cargando} = await Peticion(Global.url+"crear", "POST", nuevoArticulo);
+
+    if(datos.status === "success"){
+
+      setResultado("guardado");
+
+    }else{
+
+      setResultado("error");
+
+    }
+    console.log(datos);
+
+  }
+
+
   return (
-    <div>Crear</div>
+    <div className='jumbo'>
+      <h1>Crear articulo</h1>
+      <p>Formulario para crear articulo </p>
+     
+
+      <strong> {resultado == "guardado"? "articulo guardado con exito" : ""} </strong>
+      <strong> {resultado == "error"? "Los datos proporcionados son incorrectos" : ""} </strong>
+
+      {/*Montar formulariuoo */}
+
+      <form className='formulario' onSubmit={guardarArticulo}>
+
+        <div className='form-group'>
+          <label htmlFor='titulo'>Titulo</label>
+          <input type="text" name='titulo'  onChange={cambiado}/>
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='contenido'>contenido</label>
+          <textarea type="text" name='contenido' onChange={cambiado} />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='file0'>imagen</label>
+          <input type="file" name='file0'  id='file'/>
+        </div>
+
+        <input type="submit" value="guardar" className="btn btn-success" id="" />
+      </form>
+
+    </div>
   )
 }
---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState } from 'react'
+
+export const useForm = (objetoInicial = {}) => {
+
+
+    const[formulario, setFormulario] = useState(objetoInicial);
+
+    const serialFormulario = (formulario) => {
+
+
+        const formData = new FormData(formulario);
+
+        const objetoCompleto = {};
+
+        for(let [name, value] of formData){
+            objetoCompleto[name] = value;
+        }
+
+        return objetoCompleto;
+
+    }
+
+    const enviado = (e) => {
+        e.preventDefault();
+
+        let curso = serialFormulario(e.target);
+
+        setFormulario(curso);
+    }
+
+    const cambiado = ({target}) => {
+        const {name, value} = target;
+
+        setFormulario({
+            ...formulario,
+            [name]: value
+        })
+    }
+
+
+
+
+    return {
+        formulario,
+        enviado,
+        cambiado
+    }
+}
